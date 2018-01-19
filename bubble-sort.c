3b1deb767640c9b9d35b02cfb912e48b30342343
coldly <cold1y@163.com>
2018-01-19 16:20:06 +0800

    Update bubble-sort.c

--- bubble-sort.c
+++ bubble-sort.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 int main() {
-        int a[100], i, j, t, n;
+        struct student {
+                char name[21];
+                char score;
+        };
+        struct student a[100], t;
+        int i, j, n;
         scanf("%d", &n);
         for(i = 1; i <= n; i++)
-                scanf("%d", &a[i]);
+                scanf("%s %d", &a[i].name, &a[i].score);
         for(i =1; i < n; i++) {
                 for(j = 1; j <= n-i; j++) {
-                        if(a[j] < a[j+1]) {
+                        if(a[j].score < a[j+1].score) {
                                 t = a[j];
                                 a[j] = a[j+1];
                                 a[j+1] = t;
@@ -14,7 +19,7 @@ int main() {
                 }
         }
         for(i = 1; i <= n; i++)
-                printf("%d ", a[i]);
+                printf("%s\n", a[i].name);
         getchar();
         return 0;
 }
